8cfdfc55102d3d272b69faacaa948a723dc5f78e
Hanseul Jun <hanseuljun@gmail.com>
2022-11-14 18:06:01 -0800

    Add build.yml.

--- build.py
+++ build.py
@@ -4,6 +4,13 @@ import subprocess
 from pathlib import Path
 
 
+def cleanup_zconf_h():
+    here = Path(__file__).parent.resolve()
+    # The line below is needed not to have zlib repository as modified,
+    # which leaves an annoying message in git.
+    subprocess.run(["mv", f"{here}/zlib/zconf.h.included", f"{here}/zlib/zconf.h"])
+
+
 def build_arm64_mac_binaries():
     here = Path(__file__).parent.resolve()
     subprocess.run(["cmake",
@@ -13,14 +20,34 @@ def build_arm64_mac_binaries():
                     "-D", f"CMAKE_INSTALL_PREFIX={here}/install/arm64-mac"])
     subprocess.run(["make", "-C", f"{here}/build/arm64-mac", "-j8"])
     subprocess.run(["make", "-C", f"{here}/build/arm64-mac", "install"])
-    # The line below is needed not to have zlib repository as modified,
-    # which leaves an annoying message in git.
-    subprocess.run(["mv", f"{here}/zlib/zconf.h.included", f"{here}/zlib/zconf.h"])
+    cleanup_zconf_h()
+
+
+def build_x64_mac_binaries():
+    here = Path(__file__).parent.resolve()
+    subprocess.run(["cmake",
+                    "-S", f"{here}/zlib",
+                    "-B", f"{here}/build/x64-mac",
+                    "-D", "CMAKE_OSX_ARCHITECTURES=x86_64",
+                    "-D", f"CMAKE_INSTALL_PREFIX={here}/install/x64-mac"])
+    subprocess.run(["make", "-C", f"{here}/build/x64-mac", "-j8"])
+    subprocess.run(["make", "-C", f"{here}/build/x64-mac", "install"])
+    cleanup_zconf_h()
 
 
 def main():
+    print(f"platform.system(): {platform.system()}")
+    print(f"platform.machine(): {platform.machine()}")
+
     if platform.system() == "Darwin":
-        build_arm64_mac_binaries()
+        if platform.machine() == "arm64":
+            build_arm64_mac_binaries()
+            return
+        elif platform.machine() == "x86_64":
+            build_x64_mac_binaries()
+            return
+    
+    raise Error(f"zlib build not supported.")
 
 
 if __name__ == "__main__":
